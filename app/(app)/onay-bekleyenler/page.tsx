46094279615928419856cc109be2e0324f39aa89
sabridogruyol-kigili <sabridogruyol.kigili@gmail.com>
2026-09-08 23:41:39 +0300

    Update page.tsx

--- app/(app)/onay-bekleyenler/page.tsx
+++ app/(app)/onay-bekleyenler/page.tsx
@@ -17,7 +17,12 @@ export default async function OnayBekleyenlerPage() {
       id, onaylayici_rol_baglami,
       talep_gonderimler!inner (
         id, aciklama, norm_kontrol_sonucu,
-        talepler!inner ( talep_no, pozisyon_tipi, kisi_sayisi, acan_rol, magazalar!magaza_id(magaza_adi) )
+        talepler!inner (
+          talep_no, talep_turu, pozisyon_tipi, kisi_sayisi, acan_rol, magaza_id,
+          norm_kategorisi, norm_eski_deger, norm_yeni_deger,
+          magazalar!magaza_id(magaza_adi),
+          cikarilacak_personel:personel!cikarilacak_personel_id(id, ad_soyad, guncel_unvan)
+        )
       )
     `)
     .eq("onaylayici_kullanici_id", me.id)
@@ -30,19 +35,30 @@ export default async function OnayBekleyenlerPage() {
         <div className="text-xs text-gray-400 mt-0.5">Sizin onayınızı bekleyen talepler</div>
       </div>
       <div className="space-y-3">
-        {(onaylar ?? []).map((o: any) => (
-          <OnayKarti
-            key={o.id}
-            onayId={o.id}
-            talepNo={o.talep_gonderimler.talepler.talep_no}
-            magaza={o.talep_gonderimler.talepler.magazalar?.magaza_adi}
-            pozisyon={o.talep_gonderimler.talepler.pozisyon_tipi}
-            kisiSayisi={o.talep_gonderimler.talepler.kisi_sayisi}
-            acanRol={o.talep_gonderimler.talepler.acan_rol}
-            aciklama={o.talep_gonderimler.aciklama}
-            normSonuc={o.talep_gonderimler.norm_kontrol_sonucu}
-          />
-        ))}
+        {(onaylar ?? []).map((o: any) => {
+          const t = o.talep_gonderimler.talepler;
+          return (
+            <OnayKarti
+              key={o.id}
+              onayId={o.id}
+              talepNo={t.talep_no}
+              talepTuru={t.talep_turu}
+              magaza={t.magazalar?.magaza_adi}
+              magazaId={t.magaza_id}
+              pozisyon={t.pozisyon_tipi}
+              kisiSayisi={t.kisi_sayisi}
+              acanRol={t.acan_rol}
+              aciklama={o.talep_gonderimler.aciklama}
+              normSonuc={o.talep_gonderimler.norm_kontrol_sonucu}
+              cikarilacakPersonelId={t.cikarilacak_personel?.id}
+              cikarilacakPersonelAdi={t.cikarilacak_personel?.ad_soyad}
+              cikarilacakPersonelUnvan={t.cikarilacak_personel?.guncel_unvan}
+              normKategori={t.norm_kategorisi}
+              normEski={t.norm_eski_deger}
+              normYeni={t.norm_yeni_deger}
+            />
+          );
+        })}
         {(onaylar ?? []).length === 0 && (
           <div className="bg-white border border-gray-200 rounded-card p-6 text-center text-gray-400 text-xs">
             Onayınızı bekleyen talep yok.
