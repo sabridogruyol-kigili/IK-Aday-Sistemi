aaf375e2a54c820052f15621303f2676bb272c95
sabridogruyol-kigili <sabridogruyol.kigili@gmail.com>
2026-09-08 19:43:27 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -45,5 +45,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|gif|svg|webp|ico)$).*)"],
 };
